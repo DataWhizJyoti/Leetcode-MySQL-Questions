826e77fdc397ebe1a12a4dfa3982cac7dd261ed0
DataWhizJyoti <155168612+DataWhizJyoti@users.noreply.github.com>
2024-01-31 08:46:36 +0530

    Time: 741 ms (29.28%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+select e.employee_id
+from Employees e
+left join Employees m
+on e.manager_id=m.employee_id
+where e.salary<30000 and e.manager_id is not null and m.employee_id is null
+order by employee_id;
