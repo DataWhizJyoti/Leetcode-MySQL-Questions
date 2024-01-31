15dd305836af8b92b30ca955ee9b34119830dd90
DataWhizJyoti <155168612+DataWhizJyoti@users.noreply.github.com>
2024-01-31 08:23:17 +0530

    Time: 1345 ms (30.20%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+select user_id, time_stamp as last_stamp
+from (select user_id, time_stamp, 
+    row_number()over(partition by user_id order by time_stamp desc) as rnk
+    from Logins
+    where year(time_stamp)=2020
+) temp
+where rnk=1;
