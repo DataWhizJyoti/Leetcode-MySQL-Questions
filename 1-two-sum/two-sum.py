b1d1a68c26430adfb71c02dcedc39db87635db84
DataWhizJyoti <155168612+DataWhizJyoti@users.noreply.github.com>
2024-02-01 11:53:51 +0530

    Time: 37 ms (82.50%) | Memory: 12.5 MB (90.74%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        """ Brute Force
+            Time Complexity-O(N^2)
+            Space Complexity-O(1)
+
+        for i in range(0, len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
+        
+        Better Solution-Using hashmap
+        Time Complexity-O(N)
+        Space Complexity-O(N)"""
+
+        prevmap={}
+        for i,n in enumerate(nums):
+            diff=target-n
+            if diff in prevmap:
+                return [prevmap[diff],i]
+            else:
+                prevmap[n]=i
+        
